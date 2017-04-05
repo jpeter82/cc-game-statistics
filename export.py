9198b3c10f28dcffe1491b9b2826e7fcd06ee9eb
Peter Juhasz <jpeter82@gmail.com>
2017-04-05 13:44:16 +0200

    Separate export functions

--- export.py
+++ export.py
@@ -12,10 +12,14 @@ TITLE = config.title
 
 
 # Export functions
-def write_to_file(source_data, report_file, year, genre, title):
+def write_to_file_all(source_data, report_file, year, genre, title):
     '''
     Write package 1 reports to a file.
+        @param source_data string Name of the file that contains source data
         @param report_file string Name of the file to be created
+        @param year int Year parameter for inside function
+        @param genre string Genre parameter for inside function
+        @param title string Title parameter for inside function
         @return bool True if successful, otherwise False
     '''
     try:
@@ -37,9 +41,181 @@ def write_to_file(source_data, report_file, year, genre, title):
     return result
 
 
+def append_to_file(source_data, report_file, what):
+    '''
+    Append report to a file.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @param what string The new line to be appended to the report file
+        @return bool True if successful, otherwise False
+    '''
+    try:
+        with open(report_file, 'a', encoding='utf-8') as f:
+            f.write(str(what) + '\n')
+    except:
+        result = False
+    else:
+        result = True
+
+    return result
+
+
+def export_count_games(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.count_games(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_decide(source_data, report_file, year):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @param year int Year parameter for inside function
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.decide(source_data, year)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_get_latest(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.get_latest(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_count_by_genre(source_data, report_file, genre):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @param genre string Genre parameter for inside function
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.count_by_genre(source_data, genre)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_get_line_number_by_title(source_data, report_file, title):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @param title string Title parameter for inside function
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.get_line_number_by_title(source_data, title)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_sort_abc(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    data = ', '.join(reports.sort_abc(source_data))
+    if append_to_file(source_data, report_file, data):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_get_genres(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    data = ', '.join(reports.get_genres(source_data))
+    if append_to_file(source_data, report_file, data):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_when_was_top_sold_fps(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.when_was_top_sold_fps(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def delete_content(file_name):
+    '''
+    Delete contents of a file
+        @param file_name string Name of the file of which content will be deleted
+        @result True if successful, otherwise False
+    '''
+    try:
+        with open(file_name, "w"):
+            pass
+    except:
+        result = False
+    else:
+        result = True
+
+    return result
+
+
 def main():
-    # print(write_to_file(SOURCE_DATA, REPORT_FILE, YEAR, GENRE, TITLE))
-    write_to_file(SOURCE_DATA, REPORT_FILE, YEAR, GENRE, TITLE)
+    # write_to_file_all(SOURCE_DATA, REPORT_FILE, YEAR, GENRE, TITLE)
+
+    if delete_content(REPORT_FILE):
+        check_result = []
+
+        check_result.append(export_count_games(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_decide(SOURCE_DATA, REPORT_FILE, YEAR))
+        check_result.append(export_get_latest(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_count_by_genre(SOURCE_DATA, REPORT_FILE, GENRE))
+        check_result.append(export_get_line_number_by_title(SOURCE_DATA, REPORT_FILE, TITLE))
+        check_result.append(export_sort_abc(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_get_genres(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_when_was_top_sold_fps(SOURCE_DATA, REPORT_FILE))
+
+        if all(check_result):
+            print('Reports exported successfully!')
+        else:
+            print('An error occured. Please check your source file.')
+    else:
+        print('An error occured while deleting previous contents of the report file. \
+Please close the file and try again.')
 
 if __name__ == '__main__':
     main()
--- part2/export.py
+++ part2/export.py
@@ -12,8 +12,10 @@ TITLE = config.title
 # Export functions
 def write_to_file(source_data, report_file, title):
     '''
-    Write package 1 reports to a file.
+    Write package 2 reports to a file.
+        @param source_data string Name of the file that contains source data
         @param report_file string Name of the file to be created
+        @param title string Title parameter for inside function
         @return bool True if successful, otherwise False
     '''
     try:
@@ -36,9 +38,181 @@ def write_to_file(source_data, report_file, title):
     return result
 
 
+def append_to_file(source_data, report_file, what):
+    '''
+    Append report to a file.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @param what string The new line to be appended to the report file
+        @return bool True if successful, otherwise False
+    '''
+    try:
+        with open(report_file, 'a', encoding='utf-8') as f:
+            f.write(str(what) + '\n')
+    except:
+        result = False
+    else:
+        result = True
+
+    return result
+
+
+def export_get_most_played(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.get_most_played(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_sum_sold(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.sum_sold(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_get_selling_avg(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.get_selling_avg(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_count_longest_title(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.count_longest_title(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_get_date_avg(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    if append_to_file(source_data, report_file, reports.get_date_avg(source_data)):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_get_game(source_data, report_file, title):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @param title string Title parameter for inside function
+        @return bool True if successful, otherwise False
+    '''
+    data = ', '.join(str(item) for item in reports.get_game(source_data, title))
+    if append_to_file(source_data, report_file, data):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_count_grouped_by_genre(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    data = ', '.join((str(key) + ': ' + str(value)) for key, value
+                     in reports.count_grouped_by_genre(source_data).items())
+    if append_to_file(source_data, report_file, data):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def export_get_date_ordered(source_data, report_file):
+    '''
+    Append actual report to report file. Check out reports.py for content of actual report.
+        @param source_data string Name of the file that contains source data
+        @param report_file string Name of the file to be created
+        @return bool True if successful, otherwise False
+    '''
+    data = ', '.join(reports.get_date_ordered(source_data))
+    if append_to_file(source_data, report_file, data):
+        result = True
+    else:
+        result = False
+    return result
+
+
+def delete_content(file_name):
+    '''
+    Delete contents of a file
+        @param file_name string Name of the file of which content will be deleted
+        @result True if successful, otherwise False
+    '''
+    try:
+        with open(file_name, "w"):
+            pass
+    except:
+        result = False
+    else:
+        result = True
+
+    return result
+
+
 def main():
-    # print(write_to_file(SOURCE_DATA, REPORT_FILE, TITLE))
-    write_to_file(SOURCE_DATA, REPORT_FILE, TITLE)
+    # write_to_file(SOURCE_DATA, REPORT_FILE, TITLE)
+
+    if delete_content(REPORT_FILE):
+        check_result = []
+
+        check_result.append(export_get_most_played(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_sum_sold(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_get_selling_avg(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_count_longest_title(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_get_date_avg(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_get_game(SOURCE_DATA, REPORT_FILE, TITLE))
+        check_result.append(export_count_grouped_by_genre(SOURCE_DATA, REPORT_FILE))
+        check_result.append(export_get_date_ordered(SOURCE_DATA, REPORT_FILE))
+
+        if all(check_result):
+            print('Reports exported successfully!')
+        else:
+            print('An error occured. Please check your source file.')
+    else:
+        print('An error occured while deleting previous contents of the report file. \
+Please close the file and try again.')
 
 if __name__ == '__main__':
     main()
